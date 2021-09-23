f8b6789009cd95b7f6719729f6bd6d14d09032e5
Demir Dereli <52353085+demurthelemur@users.noreply.github.com>
2021-09-23 16:05:02 +0300

    Add files via upload

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Demir Dereli on 20.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
